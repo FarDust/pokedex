888270e9644f01274ce553edcfefffc016db548a
FarDust <gnfaundez@uc.cl>
2022-09-08 18:36:09 -0400

    chore: remove sortile name from app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sortile-pokedex'),
+      dir: require('path').join(__dirname, './coverage/pokedex'),
       subdir: '.',
       reporters: [
         { type: 'html' },
